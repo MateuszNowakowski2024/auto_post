b7155c3b9fa5c739c32366efe11af5d5cb27a6f5
Mateusz N <kleszczer@gmail.com>
2025-03-30 03:06:08 +0200

    major update v11

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ S3_VIDEO_KEY = "videos/columns_v1.mp4"
 # Automatically get the base directory of the project
 
 OUTPUT_FILE = os.path.abspath("auto_post_reels/temp/temp_video.mp4") # os.path.join(BASE_DIR, "auto_post_reels", "temp", "temp_video.mp4")
-AUDIO_PATH = os.path.abspath("auto_post/auto_post_reels/reel_sounds") #os.path.join(BASE_DIR, "auto_post_reels", "reel_sounds")
-FONT_PATH = os.path.abspath("auto_post/auto_post_reels/reel_fonts/SuperCaramel-5yBza.ttf") #os.path.join(BASE_DIR, "auto_post_reels", "reel_fonts", "SuperCaramel-5yBza.ttf")
+AUDIO_PATH = os.path.abspath("auto_post_reels/reel_sounds") #os.path.join(BASE_DIR, "auto_post_reels", "reel_sounds")
+FONT_PATH = os.path.abspath("auto_post_reels/reel_fonts/SuperCaramel-5yBza.ttf") #os.path.join(BASE_DIR, "auto_post_reels", "reel_fonts", "SuperCaramel-5yBza.ttf")
 
 # Video settings
 WIDTH = 720
